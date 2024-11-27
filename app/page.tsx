8bd340d92381d9c721cbfef45df98799823b2d1f
Julien <julien.milants@gmail.com>
2024-11-27 18:37:16 +0100

    init: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,145 @@
+'use client'
+import { useEffect } from "react";
+import Navbar from "@/src/components/Navbar";
+import Link from "next/link";
 import Image from "next/image";
-
+import LogoWhite from "@/src/img/cei-logo-header.png";
+import LogoBlack from "@/src/img/cei_logo_sansfond.svg";
+import { gsap } from "gsap"; // Import GSAP
+import { FaFacebook, FaInstagram } from "react-icons/fa";
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  useEffect(() => {
+    // GSAP animations for page elements
+    gsap.fromTo(
+      ".hero-title",
+      { opacity: 0, y: -50 }, // Starting state
+      { opacity: 1, y: 0, duration: 1, ease: "power3.out" } // Ending state
+    );
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    gsap.fromTo(
+      ".hero-description",
+      { opacity: 0, x: -100 },
+      { opacity: 1, x: 0, duration: 1, delay: 0.2, ease: "power3.out" }
+    );
+
+    gsap.fromTo(
+      ".hero-button",
+      { opacity: 0, y: 50 },
+      { opacity: 1, y: 0, duration: 1, delay: 0.4, ease: "power3.out", stagger: 0.3 }
+    );
+
+    gsap.fromTo(
+      ".section-title",
+      { opacity: 0, y: 50 },
+      { opacity: 1, y: 0, duration: 1, delay: 0.6, ease: "power3.out" }
+    );
+
+    gsap.fromTo(
+      ".section-text",
+      { opacity: 0, x: -100 },
+      { opacity: 1, x: 0, duration: 1, delay: 0.8, ease: "power3.out" }
+    );
+
+    gsap.fromTo(
+      ".logo",
+      { opacity: 0, scale: 0.5 },
+      { opacity: 1, scale: 1, duration: 1, delay: 1, ease: "power3.out" }
+    );
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <div className="bg-white dark:bg-gray-900 min-h-screen flex flex-col justify-center items-center">
+        {/* Section principale */}
+        <div className="flex flex-col items-center text-center px-4 py-10">
+          <h1 className="hero-title text-black dark:text-white text-3xl sm:text-5xl font-bold">
+            Cercle des Étudiants en Informatique
+          </h1>
+          <p className="hero-description text-black dark:text-white mt-4 text-base sm:text-lg max-w-2xl">
+            Nous sommes une association d’étudiants des départements de
+            cybersécurité, développement d’applications et intelligence
+            artificielle à HELMo.
+          </p>
+          <div className="flex flex-col justify-center items-center sm:flex-row gap-4 mt-6">
+            <Link href="/pages/event">
+              <button className="hero-button px-6 py-3 rounded-lg bg-blue-500">
+                Événements
+              </button>
+            </Link>
+            <Link href="/pages/tutoring">
+              <button className="hero-button px-6 py-3 rounded-lg bg-gray-500">
+                Tutorat
+              </button>
+            </Link>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        {/* Section "Qui sommes-nous" */}
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-10 px-4 lg:px-20 py-10 w-full mt-16 sm:mt-24 md:mt-32">
+          <div className="lg:w-1/2">
+            <h2 className="section-title text-black dark:text-white text-2xl sm:text-4xl font-bold">
+              Qui sommes-nous
+            </h2>
+            <p className="section-text text-black dark:text-white mt-4 text-base sm:text-lg">
+              Le Cercle Étudiant en Informatique <span className="font-bold">(CEI)</span> est une association
+              étudiante dédiée aux étudiants en informatique. Notre mission est de
+              créer une communauté dynamique et enrichissante pour tous les passionnés
+              d&#39;informatique.
+            </p>
+            <p className="section-text text-black dark:text-white mt-4 text-base sm:text-lg">
+              Nous organisons régulièrement des événements, des sessions de tutorat
+              et des activités pour favoriser l&#39;apprentissage et les échanges
+              entre étudiants.
+            </p>
+          </div>
+          <div className="flex justify-center lg:w-1/2">
+            <div className="block dark:hidden logo">
+              <Image src={LogoBlack} alt="Logo CEI Light" width={400} height={200} />
+            </div>
+            <div className="hidden dark:block logo">
+              <Image src={LogoWhite} alt="Logo CEI Dark" width={400} height={200} />
+            </div>
+          </div>
+        </div>
+
+        {/* Section Newsletter */}
+        <div className="text-black dark:text-white w-full py-12 mt-16">
+          <div className="flex flex-col items-center px-4">
+            <h3 className="text-3xl font-bold mb-4">Abonnez-vous à notre newsletter</h3>
+            <p className="text-lg mb-6">Restez à jour avec nos événements et nos activités.</p>
+            <form className="flex items-center w-full max-w-sm ">
+              <input
+                type="email"
+                placeholder="Votre adresse email"
+                className="px-4 py-2 rounded-l-lg text-black w-full focus:outline-none border-solid border border-black dark:border-none"
+                required
+              />
+              <button
+                type="submit"
+                className="px-6 py-2 bg-blue-500 text-white rounded-r-lg hover:bg-blue-600"
+              >
+                S&#39;abonner
+              </button>
+            </form>
+          </div>
+        </div>
+
+        {/* Footer */}
+        <footer className="text-black dark:text-white w-full flex items-center justify-between p-6">
+          <div className="text-center mt-4 text-sm">
+            <p>&copy; 2024 Cercle des Étudiants en Informatique. Tous droits réservés.</p>
+          </div>
+          <div className="flex justify-center items-center space-x-8">
+            <Link href="https://facebook.com" target="_blank">
+              <FaFacebook className="w-6 h-6 text-blue-600 hover:text-blue-800 transition duration-200" />
+            </Link>
+            <Link href="https://instagram.com" target="_blank">
+              <FaInstagram className="w-6 h-6 text-pink-600 hover:text-pink-800 transition duration-200" />
+            </Link>
+          </div>
+        </footer>
+      </div>
+    </>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,107 @@
+"use client";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { FaBars, FaTimes, FaMoon, FaSun } from "react-icons/fa";
+import LogoBlack from "@/src/img/cei_logo_sansfond.svg";
+import LogoWhite from "@/src/img/cei-logo-header.png";
+
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // Basculer le mode sombre
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+    if (isDarkMode) {
+      document.documentElement.classList.remove("dark");
+    } else {
+      document.documentElement.classList.add("dark");
+    }
+  };
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme === "dark") {
+      setIsDarkMode(true);
+      document.documentElement.classList.add("dark");
+    } else {
+      setIsDarkMode(false);
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("theme", isDarkMode ? "dark" : "light");
+  }, [isDarkMode]);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className="bg-white dark:bg-gray-900 shadow-2xl">
+      <div className="px-8">
+        <div className="flex justify-between items-center h-16">
+          {/* Logo */}
+          <div className="flex items-center">
+            <Link href="/">
+              <div className="flex items-center cursor-pointer">
+                <div className="block dark:hidden">
+                  <Image src={LogoBlack} alt="Logo CEI Light" height={25} />
+                </div>
+                <div className="hidden dark:block">
+                  <Image src={LogoWhite} alt="Logo CEI Dark" height={25} />
+                </div>
+              </div>
+            </Link>
+          </div>
+          <div className="hidden md:flex space-x-6">
+            <Link
+              href="/tutorat"
+              className="text-gray-800 text-lg dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 font-bold"
+            >
+              Tutorat
+            </Link>
+            <Link
+              href="/evenements"
+              className="text-gray-800 text-lg dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 font-bold"
+            >
+              Événements
+            </Link>
+          </div>
+          <div className="flex items-center space-x-4">
+            <button
+              onClick={toggleDarkMode}
+              className="text-gray-800 dark:text-gray-200 focus:outline-none"
+            >
+              {isDarkMode ? <FaSun size={20} /> : <FaMoon size={20} />}
+            </button>
+            <button
+              onClick={toggleMenu}
+              className="text-gray-800 dark:text-gray-200 focus:outline-none md:hidden"
+            >
+              {isMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+            </button>
+          </div>
+        </div>
+      </div>
+      {isMenuOpen && (
+        <div className="md:hidden flex flex-col items-center space-y-2 pt-2 pb-3 bg-white dark:bg-gray-900">
+          <Link
+            href="/tutorat"
+            className="text-gray-800 text-lg dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 font-bold"
+          >
+            Tutorat
+          </Link>
+          <Link
+            href="/evenements"
+            className="text-gray-800 text-lg dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 font-bold"
+          >
+            Événements
+          </Link>
+        </div>
+      )}
+    </nav>
+  );
+}
